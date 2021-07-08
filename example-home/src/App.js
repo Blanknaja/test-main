6fcd35992b2610b4d210c7ae8bfd3d93012d25ca
athibet <>
2021-07-08 14:12:49 +0700

    use ts config

--- example-home/src/App.js
+++ example-home/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+// import { Button } from '../lib/module1/build/index'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>test src/App.js</code> and save to reload.
         </p>
+        {/* <Button textTitle = "test src/App.js" /> */}
         <a
           className="App-link"
           href="https://reactjs.org"
