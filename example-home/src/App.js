1111f4870c709512b1bfdc883f4cc5f24074675b
ATHIBET <athibet@home.co.th>
2021-07-11 21:39:05 +0700

    update code

--- example-home/src/App.js
+++ example-home/src/App.js
@@ -5,27 +5,14 @@ import './App.css';
 import  { Hello } from 'home-component-modules';
 import {ProjectCardVertical}   from 'home-component-modules';
 
-
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>test src/App.js</code> and save to reload.
-        </p>
+
         {/* <Button textTitle = "test src/App.js" /> */}
         <Hello text = "test Message"/>
         <ProjectCardVertical title = "HI CARD"/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
